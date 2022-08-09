610422c4596ff92084fc0bf83fd897f9dc3b28ae
Cengizhan Tomak <cengizhantomak@gmail.com>
2022-08-09 15:51:04 +0300

    12 Verileri Gostermek

--- FotografPaylasmaUygulamasi/FeedViewController.swift
+++ FotografPaylasmaUygulamasi/FeedViewController.swift
@@ -6,10 +6,16 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var emailDizisi = [String]()
+    var yorumDizisi = [String]()
+    var gorselDizisi = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,16 +24,51 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        firebaseVerileriAl()
     }
     
+    func firebaseVerileriAl() {
+        let firestoreDatabase = Firestore.firestore()
+        
+        firestoreDatabase.collection("Post").addSnapshotListener { (snapshot, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    for document in snapshot!.documents {
+                        //let documentId = document.documentID
+                        
+                        if let gorselUrl = document.get("gorselurl") as? String {
+                            self.gorselDizisi.append(gorselUrl)
+                        }
+                        
+                        if let yorum = document.get("yorum") as? String {
+                            self.yorumDizisi.append(yorum)
+                        }
+                        
+                        if let email = document.get("email") as? String {
+                            self.emailDizisi.append(email)
+                        }
+                        
+                    }
+                    
+                    self.tableView.reloadData()
+                    
+                }
+            }
+        }
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emailDizisi.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.emailText.text = "tuna@gmail.com"
-        cell.yorumText.text = "benim yorumum"
+        cell.emailText.text = emailDizisi[indexPath.row]
+        cell.yorumText.text = yorumDizisi[indexPath.row]
         cell.postImageView.image = UIImage(named: "gorselsec")
         return cell
     }
